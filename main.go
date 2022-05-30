708e00e4ec6c59d5deeb10c50a893c9a959d4922
zhangshuhao <2905154639@qq.com>
2022-05-30 20:03:22 +0800

    第一次提交

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// test
+	fmt.Println(Reverse("abcd"))
+}
+
+// 字符串反转
+func Reverse(s string) string {
+	b := []byte(s)
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b)
+}
