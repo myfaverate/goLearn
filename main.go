b77a754e0002e93e77b7b19a789ab02aa3ab8552
zhangshuhao <2905154639@qq.com>
2022-05-30 23:06:35 +0800

    "模糊测试"

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	// test
 	input:="abcdef省赛ghigklmn"
-	rev:=Reverse(input)
-	doubleRev:=Reverse(rev)
+	rev,_:=Reverse(input)
+	doubleRev,_:=Reverse(rev)
 	fmt.Printf("原始字符串: %q\n", input)
 	fmt.Printf("反转之后的字符串%q\n",rev)
 	fmt.Printf("双重反转之后的字符串%q\n",doubleRev)
@@ -14,10 +18,16 @@ func main() {
 
 // 字符串反转 更改
 // 第三次更改
-func Reverse(s string) string {
+func Reverse(s string) (string,error){
+	if !utf8.ValidString(s) {
+		return s,errors.New("输入不是一个utf-8字符")
+	}
+	fmt.Printf("input %q\n", s)
 	b := []rune(s)
+	fmt.Printf("runes %q\n", b)
+	// b := []byte(s)
 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
 	}
-	return string(b)
+	return string(b),nil
 }
--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	// "fmt"
 	"testing"
+	"unicode/utf8"
 )
 
 func TestMain(t *testing.T) {
@@ -12,13 +13,30 @@ func TestMain(t *testing.T) {
 		{"hello,world", "dlrow,olleh"},
 		{" ", " "},
 		{"!123456", "654321!"},
-		{"张书豪", "豪书张"},
+		// {"张书豪", "豪书张"},
 	}
 	for _, tc := range testcases {
-		rev:=Reverse(tc.in)
+		rev,_:=Reverse(tc.in)
 		if rev!=tc.want {
 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
 		}
 	}
 	//fmt.Println("测试成功！")
+}
+func FuzzMain(f *testing.F) {
+	testcases:=[]string{"Hello,World!", " ", "!123456789", "张书豪", "∑σΣ"}
+	// testcases:=[]string{"Hello,World!", " ", "!123456789"}
+	for _, tc := range testcases {
+		f.Add(tc)
+	}
+	f.Fuzz(func(t *testing.T, a string){
+		rev,_:=Reverse(a)
+		doubleRev,_:=Reverse(rev)
+		if a!=doubleRev {
+			t.Errorf("Reverse: %q, want %q", a, doubleRev)
+		}
+		if utf8.ValidString(a)&&!utf8.ValidString(rev) {
+			t.Errorf("Reverse 之后是 非utf-8 字符串！%q",rev)
+		}
+	})
 }
